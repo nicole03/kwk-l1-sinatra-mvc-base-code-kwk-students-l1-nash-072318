155f66d321e563e2c1ca5fe75736fcffe2fbc257
nicole03 <ntidwell03@gmail.com>
2018-07-31 19:51:34 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -4,7 +4,18 @@ Bundler.require
 class MyApp < Sinatra::Base
 
   get '/' do
+   
+    @num1=5
+    @num2=9
     erb :index
+ 
   end
-
+ get '/tacos' do
+   @b=4
+    erb :tacos
+  end
+ get '/rainbows' do
+   
+    erb :rainbows
+end
 end
